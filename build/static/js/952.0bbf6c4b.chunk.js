52389ffd41f7145e8a5733f97ef4e8fb32382b9f
clmi@ucla.edu <clmi@ucla.edu>
2025-04-12 17:51:21 -0700

    Add build folder for Netlify deployment

---
+++ build/static/js/952.0bbf6c4b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwiser_website=self.webpackChunkwiser_website||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0bbf6c4b.chunk.js.map
